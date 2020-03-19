6cd7c6be0109631fb8501da3032c84dda9efe74c
anshuln <anshulnasery@gmail.com>
2020-03-19 13:38:16 +0530

    Adds stuff to a database

--- WhatsApp/ClosingPounce/parser.py
+++ WhatsApp/ClosingPounce/parser.py
@@ -2,12 +2,20 @@
 #TODOs - 1. Extract messages -- DONE
 #        2. Decide if they are questions -- DONE
 #        3. Save into an object with timestamp, author, question number and                     question, with a pretty printer function -- DONE
-#        4. Upload to required thing
+#        4. Upload to required thing -- DONE for slides
+#        5. Write a driver file
+#        6. Put stuff in a db -- DONE
+#        7. Scrape whatsapp in a different way
 FILENAME = 'messages.txt'
+DB       = 'questions.sqlite'
 NUMBER   = 1
 import re
+import sqlite3
 from upload import Slides_uploader
+
 class Question():
+	#TODO get max question number
+	#TODO init from database
 	number = NUMBER
 	def __init__(self,match):
 		self.date     = match.group('date')
@@ -33,14 +41,25 @@ class Question():
 			return True  
 		return False
 	def __str__(self):
+		#TODO add different stringers for MD, HTML, blogs etc
 		string = "Number:{}\nDate:{}\nTime:{}\nUser:{}\nMessage:{}\n".format(self.qnumber,self.date,self.time,self.QM,self.message)
 		return string
+
+	def update_db(self,cursor):
+		sqlite_insert_with_param = """INSERT INTO 'questions'
+		                          ('Number','Date','Time','QM','Question') 
+		                          VALUES (?, ?, ?,?,?);"""
+		data = (self.qnumber,self.date,self.time,self.QM,self.message)	
+		cursor.execute(sqlite_insert_with_param, data)
 
 def extract_messages(text):
 	'''
 	Given a text, extracts all messages into a list of strings
 	''' 
-	sl = Slides_uploader(presentation_id)
+	sqliteConnection = sqlite3.connect('questions.db')
+	cursor = sqliteConnection.cursor()
+
+	# sl = Slides_uploader(presentation_id)
 	Question.number = NUMBER 
 	file = open("Rejects.txt","w")
 	regex_pattern = re.compile(r'^(?P<date>\d{2}\/\d{2}\/\d{4})\, (?P<time>\d{2}\:\d{2}) \- (?P<QM>[\w\+ ]+)\: (?P<message>[\s\S]+?)(?=^\d{2}|\Z)',re.MULTILINE)
@@ -56,10 +75,11 @@ def extract_messages(text):
 			print(str(q))
 			print("\n______\n")
 			Question.number += 1
-			sl.upload_question(q)
+			q.update_db(cursor)
 		else:
 			file.write("\n______\n")
 			file.write(q.message)
+	sqliteConnection.commit()
 text = open(FILENAME,"r").read()
 # print(text)
 extract_messages(text)
